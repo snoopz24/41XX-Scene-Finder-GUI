15a59953811fc21345334126bef0127200e847ab
snoopz24 <35700082+snoopz24@users.noreply.github.com>
2018-01-22 13:11:56 -0500

    Add files via upload
    Different text on the button

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@ int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
 
-    QPushButton button ("Hello world !");
+    QFont font ("Courier");
+    //QFont (const Qstring & family, int pointSize = -1, int weight = -1, bool italic = false);
+
+    QPushButton button;
+    button.setFont(font);
+    button.setText("Snoopz is the greatest ever!");
+    button.setToolTip("This shit don't lie");
     button.show();
 
     return app.exec();
